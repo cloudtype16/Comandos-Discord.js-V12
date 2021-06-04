e3db36308b952518bbfc06663fa47cded6c6bdbf
Azúcar Morena <75637430+HugoMelendez0403@users.noreply.github.com>
2021-06-04 11:45:07 -0500

    Update howgay.js

--- howgay.js
+++ howgay.js
@@ -1,1 +1,24 @@
+const Discord = require("discord.js")
+const { Aleatorio } = require("mr.pajas2.0")
 
+module.exports = {
+  commands: ["howgay", "gay"],
+  expectedArgs: "[@user || id]",
+  permissionError: "",
+  minArgs: 0,
+  maxArgs: 1,
+  callback: async (message, args, text, client) => {
+      const user = message.mentions.members.first() || message.guild.members.cache.get(args.join(" ")) || message.member
+    let random = Aleatorio(100, 0, 100, true)
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`🏳️‍🌈 ${user.displayName} 🏳️‍🌈`)
+    .setDescription(`**Tu eres ${random}% gay**`)
+    .setColor(client.config.color)
+
+    const messageToDelete = await message.channel.send(embed)
+
+    Borrar(message, messageToDelete, 0); 
+  },
+  permissions: "",
+  requiredRoles: [],
+}
